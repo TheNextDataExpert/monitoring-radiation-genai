4a6e591bfcf5f05429f9878006b2200e995ba177
Landung <landungmalikantoro@gmail.com>
2025-09-03 07:08:44 +0700

    feat: Add new forms and overhaul UI/UX

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby5YQ3IgzEZwIea6dzO4wZsyDBJgr2P-IsG6xSijKzNXtV3ETrW-NO3wjC5eAB5X6JB/exec';
+const SCRIPT_URL_TG = 'https://script.google.com/macros/s/AKfycbz6dK8a__pUfOGMgHmNWftfmWfDKro3rEuQCC_BYBXl9ljy7FBXWVi51ArNgrvYbZGY/exec';
+const SCRIPT_URL_PD = 'https://script.google.com/macros/s/AKfycbwgNEOfUtpWoL9oeWsWj5LWBVuk1DEdiuNpm7rOvDC_D__5g6Sj74Sf1l5BL45IZVOu/exec';
 const USERNAME = 'operator';
 const PASSWORD = 'operator';
